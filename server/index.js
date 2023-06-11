3e108bba9f619e9366115265a5bac96224ee3158
Farhan <mohd.farhan.sg@gmail.com>
2023-06-11 14:39:43 +0800

    added script to initialise sequelize instance

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-app.use(express.json());
-
+const db = require("./models");
 const app = express();
+
+app.use(express.json());
 app.use(cors());
 // Simple Route
 app.get("/", (req, res) => {
@@ -10,7 +11,10 @@ app.get("/", (req, res) => {
 });
 require("dotenv").config();
 
-let port = process.env.APP_PORT;
-app.listen(port, () => {
-  console.log(`⚡ Sever running on http://localhost:${port}`);
+// synchronizes data models with db tables
+db.sequelize.sync({ alter: true }).then(() => {
+  let port = process.env.APP_PORT;
+  app.listen(port, () => {
+    console.log(`⚡ Sever running on http://localhost:${port}`);
+  });
 });
