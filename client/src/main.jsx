daeae853f710d7953f147c535c1b4b2791ea231a
Farhan <mohd.farhan.sg@gmail.com>
2023-07-22 00:11:05 +0800

    added toast service to app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import ToastProvider from "./components/toast/ToastProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </React.StrictMode>
 );
