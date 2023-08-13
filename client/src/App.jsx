b1453d409ba5a4e1bf39bcfe8514e914d54a2f40
mcbobert <russelllim2005@gmail.com>
2023-08-13 18:00:00 +0800

    Added Login and Register UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import LandingPage from "./pages/landing-page/LandingPage";
 import {
   createBrowserRouter,
@@ -6,6 +7,8 @@ import {
   RouterProvider,
   Route,
 } from "react-router-dom";
+import UserContext from "./context/UserContext";
+import http from "./http";
 
 // pages
 import StaffSidebar from "./components/staff-sidebar/StaffSidebar";
@@ -15,16 +18,18 @@ import Register from "./pages/login/Register";
 import Locations from "./pages/itinerary-staff/Locations";
 import Attractions from "./pages/itinerary-staff/Attractions";
 import Users from "./pages/acct-management/Users";
+import ViewUser from "./pages/acct-management/ViewUser";
 import Staff from "./pages/acct-management/Staff";
 import EditStaff from "./pages/acct-management/EditStaff";
 import EditLocations from "./pages/itinerary-staff/EditLocations";
 import EditAttractions from "./pages/itinerary-staff/EditAttractions";
 import Error from "./components/Error";
+import UserProfile from "./pages/acct-management/UserProfile";
+import UserEditProfile from "./pages/acct-management/UserEditProfile";
 
 // layouts
 import Itinerary from "./layouts/Itinerary";
 import Profiles from "./layouts/Profiles";
-import EditStaffs from "./pages/acct-management/EditStaff";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -32,7 +37,12 @@ const router = createBrowserRouter(
       <Route path="/" element={<LandingPage />} />
       <Route path="login" element={<Login />} />
       <Route path="register" element={<Register />} />
-      <Route path="user" element={<UserSidebar />}></Route>
+
+      <Route path="user" element={<UserSidebar />}>
+        <Route path="profile" element={<UserProfile />} />
+        <Route path="profile/edit" element={<UserEditProfile />} />
+      </Route>
+
       <Route path="staff" element={<StaffSidebar />}>
         <Route path="itinerary" element={<Itinerary />}>
           <Route path="locations" element={<Locations />} />
@@ -46,17 +56,31 @@ const router = createBrowserRouter(
           <Route path="staff" element={<Staff />}></Route>
         </Route>
         <Route path="profiles/staff/:id" element={<EditStaff />} />
+        <Route path="profiles/users/:id" element={<ViewUser />} />
       </Route>
+
       <Route path="*" element={<Error />} />
     </Route>
   )
 );
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      http.get("/user/auth").then((res) => {
+        setUser(res.data.user);
+      });
+    }
+  }, []);
+
   return (
-    <div className="w-full min-h-screen bg-seashell dark:bg-grey p-0 m-0">
-      <RouterProvider router={router} />
-    </div>
+    <UserContext.Provider value={{ user, setUser }}>
+      <div className="w-full min-h-screen bg-seashell dark:bg-grey p-0 m-0">
+        <RouterProvider router={router} />
+      </div>
+    </UserContext.Provider>
   );
 }
 
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import http from "../../http";
+
+import { RxArrowLeft } from "react-icons/rx";
+import { BsEyeSlash, BsEye } from "react-icons/bs";
+import DarkModeLogo from "../../assets/logoDarkMode.png";
+import UserContext from "../../context/UserContext";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const navigate = useNavigate();
+  const { setUser } = useContext(UserContext);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   if (
     localStorage.theme === "dark" ||
     (!("theme" in localStorage) &&
@@ -11,32 +29,191 @@ const Login = () => {
   } else {
     document.documentElement.classList.remove("dark");
   }
+
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .trim()
+      .email()
+      .max(50)
+      .email("Invalid email")
+      .required("Email is required"),
+    password: yup
+      .string()
+      .min(8, "Password must have at least 8 characters.")
+      .max(50)
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)/,
+        "Password must contain at least one lowercase letter, one uppercase letter, one number, and one symbol."
+      )
+      .required("Invalid password."),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (data) => {
+      data.email = data.email.trim().toLowerCase();
+      data.password = data.password.trim();
+      http
+        .post("/user/login", data)
+        .then((res) => {
+          localStorage.setItem("accessToken", res.data.accessToken);
+          setUser(res.data.user);
+          navigate("/user/profile");
+          window.location.reload();
+        })
+        .catch(function (err) {
+          console.log("Error", err.response);
+        });
+    },
+  });
+
   return (
     <>
-      <div className="w-full h-screen grid grid-rows-2 justify-items-center">
+      <div class="md:flex md:items-center md:gap-12">
+        <a href="/">
+          <img
+            src={DarkModeLogo}
+            alt="CycleCity"
+            className="w-20 h-20"
+            style={{ marginLeft: "40px", marginTop: "10px" }}
+          />
+        </a>
+      </div>
+
+      <div
+        style={{
+          position: "sticky",
+          top: "0px",
+          right: "20px",
+          display: "flex",
+          justifyContent: "flex-end",
+          paddingRight: "50px",
+          paddingTop: "-50px",
+        }}
+      >
+        <Link to={"/"}>
+          <button>
+            <RxArrowLeft size={30} color="white" />
+          </button>
+        </Link>
+      </div>
+      <div
+        className="w-full grid grid-rows-2 justify-items-center"
+        style={{ marginTop: "-140px" }}
+      >
         <h1 className="text-5xl text-grey dark:text-seashell mt-40">Login</h1>
-        <div>
-          <div className="flex gap-4">
-            <div className="flex">
-              <Link to="/staff">
-                <a className="py-2.5 px-5 bg-thistle text-grey dark:hover:text-seashell border border-thistle rounded-lg hover:bg-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">
-                  Login
-                </a>
-              </Link>
+
+        <div className="">
+          <form onSubmit={formik.handleSubmit}>
+            <div className="flex gap-4">
+              <div className="w-full relative mt-2">
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  style={{ color: "seashell" }}
+                  className={`peer pl-4 pr-10 py-3 w-full border-2 bg-grey border-fedora placeholder-transparent rounded-xl hover:border-thistle/90 focus:outline-none focus:border-thistle/60 transition-colors ${
+                    formik.touched.email && formik.errors.email
+                      ? "border-red-500"
+                      : ""
+                  }`}
+                  placeholder="Email"
+                />
+                <label
+                  htmlFor="emaiL"
+                  className={`absolute left-0 ml-4 px-1 rounded -top-2.5 text-fedora bg-grey text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3.5 peer-focus:-top-2.5 peer-focus:text-thistle/80 peer-hover:text-thistle peer-focus:text-sm ${
+                    formik.touched.email && formik.errors.email
+                      ? "text-red-500"
+                      : ""
+                  }`}
+                >
+                  Email
+                </label>
+                {formik.touched.name && formik.errors.name && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {formik.errors.email}
+                  </div>
+                )}
+              </div>
+
+              <br></br>
+              <br></br>
+
+              <div className="w-full relative mt-2 flex gap-4">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.name}
+                  style={{ color: "seashell" }}
+                  className={`peer pl-4 pr-10 py-3 w-full border-2 bg-grey border-fedora placeholder-transparent rounded-xl hover:border-thistle/90 focus:outline-none focus:border-thistle/60 transition-colors ${
+                    formik.touched.password && formik.errors.password
+                      ? "border-red-500"
+                      : ""
+                  }`}
+                  placeholder="Password"
+                />
+
+                <label
+                  htmlFor="password"
+                  className={`absolute left-0 ml-4 px-1 rounded -top-2.5 text-fedora bg-grey text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3.5 peer-focus:-top-2.5 peer-focus:text-thistle/80 peer-hover:text-thistle peer-focus:text-sm ${
+                    formik.touched.password && formik.errors.password
+                      ? "text-red-500"
+                      : ""
+                  }`}
+                >
+                  Password
+                </label>
+                <span
+                  className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                  onClick={togglePasswordVisibility}
+                  style={{ color: "white", top: "50%", left: "90%" }} // Adjust the value here
+                >
+                  {showPassword ? <BsEyeSlash /> : <BsEye />}
+                </span>
+              </div>
             </div>
-            <div className="flex">
-              <Link to="/user">
-                <a className="py-2.5 px-5 bg-transparent text-grey dark:text-seashell border border-ultraViolet rounded-lg hover:bg-ultraViolet hover:text-seashell hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">
-                  Register
-                </a>
-              </Link>
+
+            <br></br>
+
+            <div className="flex justify-center space-x-10">
+              <div>
+                <button
+                  className="py-2.5 px-5 bg-thistle text-grey dark:hover:text-seashell border-2 border-thistle rounded-lg hover:bg-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0"
+                  type="submit"
+                >
+                  Log In
+                </button>
+              </div>
             </div>
+          </form>
+
+          <br></br>
+
+          <div className="flex flex-column justify-center">
+            <h2 className="text-small text-seashell">Don't have an account?</h2>
+
+            <br></br>
+            <br></br>
           </div>
-          <div className="m-10">
-            <Link to="/">
-              <a className="py-2.5 px-8 bg-transparent text-grey dark:text-seashell border border-ultraViolet rounded-lg hover:bg-ultraViolet hover:text-seashell hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">
-                Back
-              </a>
+
+          <div className="flex justify-center">
+            <Link to={"/register"}>
+              <button
+                className="py-2.5 px-5 bg-thistle text-grey dark:hover:text-seashell border-2 border-thistle rounded-lg hover:bg-transparent transition ease-in duration-200 trans   hover:-translate-y-1 active:translate-y-0"
+                type="submit"
+              >
+                Sign Up
+              </button>
             </Link>
           </div>
         </div>
