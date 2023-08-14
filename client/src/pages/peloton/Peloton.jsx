7acc3653fd9fb82dcf4d753528cfc6792e029216
happychinaman <116698968+happychinaman@users.noreply.github.com>
2023-08-14 20:54:45 +0800

    updated components for user page of peloton

---
+++ client/src/pages/peloton/Peloton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PelotonTabs from "./PelotonTabs";
+import PelotonJoin from "./PelotonJoin";
+
+export default function Peloton() {
+  return (
+    <div className="w-screen h-screen p-16">
+      <PelotonTabs />
+    </div>
+  );
+}
+//<div class="w-full mx-auto p-2 text-gray-800 dark:text-seashell/90 relative overflow-hidden min-w-full max-w-3xl"></div>;
